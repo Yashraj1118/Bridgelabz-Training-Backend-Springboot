684e79d947335e41c5865e6dbc7547ade4a0f0ec
Yashraj1118 <yash.raj17182003@gmail.com>
2026-06-03 12:56:17 +0530

    Initialize default timezone in FundooNotesApplication
    Set default timezone to Asia/Kolkata during initialization.

--- src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
+++ src/main/java/com/fundoonotes/fundoo_notes/FundooNotesApplication.java
@@ -1,12 +1,21 @@
 package com.fundoonotes.fundoo_notes;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class FundooNotesApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		System.out.println("Default timezone initialized to Asia/Kolkata (IST)");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundooNotesApplication.class, args);
